09bfcd9051ca7462288f77d6a6e80c3e6f726079
Roger Lopez <rllopez0410@gmail.com>
2024-01-28 15:37:36 -0500

    created a responsive and interactive side bar that opens on command which will store css attributes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import  "./index.scss";
+
+const closeNav = document.getElementById("close-btn");
+const openNav = document.getElementById("open-nav");
+const sideNav = document.getElementById("side-nav");
+
+closeNav.addEventListener("click", closeSideNav);
+openNav.addEventListener("click", openSideNav);
+
+function closeSideNav() {
+    sideNav.style.width = "0";
+    sideNav.style.paddingLeft = "0";
+
+}
+
+function openSideNav() {
+    sideNav.style.width = "240px";
+    sideNav.style.paddingLeft = "10px";
+}
