3b81ce7de17ab9a17848ff65c42cf87fff0b4f63
Roger Lopez <rllopez0410@gmail.com>
2024-01-30 16:56:37 -0500

    refactoring some JS to allow me to program the group of attribute buttons as an array for better scalability

--- src/index.js
+++ src/index.js
@@ -3,31 +3,55 @@ import  "./index.scss";
 const forgeBtn = document.getElementById("open-nav");
 const sideNav = document.getElementById("side-nav");
 const toolbox = document.getElementById("toolbox");
-const displayAttr = document.getElementById("display-attribute");
-const displayVal = document.getElementById("display-values");
+const attributeBtns = document.querySelectorAll(".attribute-btns");
 
-let click = false;
 
+let toolboxClick = false;
+let cssAttribute = false;
+
+attributeBtns.forEach(attrBtns);
 forgeBtn.addEventListener("click", attributeList);
-displayAttr.addEventListener("click", cssDisplay);
+// displayAttr.addEventListener("click", cssDisplay);
 
 function attributeList() {
-    if (!click) {
+    if (!toolboxClick) {
         sideNav.style.width = "240px";
         toolbox.style.left = "250px";
-        console.log("click");
-        click = true;
+        toolboxClick = true;
     } else {
         sideNav.style.width = "0";
         sideNav.style.paddingLeft = "0";
         toolbox.style.left = "30px";    
-        click = false;
-    }
-    
+        toolboxClick = false;
+    }   
 }
 
-function cssDisplay() {
-    displayVal.style.display = "block";
-    displayAttr.style.padding = "5px 0 0px 0px";
-    displayVal.style.maxHeight = "200px";
+function attrBtns(element) {
+    element.addEventListener("click", () => {
+        console.log("clicked");
+        if (!cssAttribute) {
+            displayVal.style.display = "block";
+            displayAttr.style.padding = "5px 0 0px 0px";
+            displayVal.style.maxHeight = "200px";
+            cssAttribute = true;
+        } else {
+            displayVal.style.display = "block";
+            displayAttr.style.padding = "5px 0 0 0";
+            displayVal.style.maxHeight = "0";
+            cssAttribute = false;
+        }
+    });
 }
+// function cssDisplay() {
+//     if (!cssAttribute) {
+//         displayVal.style.display = "block";
+//         displayAttr.style.padding = "5px 0 0px 0px";
+//         displayVal.style.maxHeight = "200px";
+//         cssAttribute = true;
+//     } else {
+//         displayVal.style.display = "block";
+//         displayAttr.style.padding = "5px 0 0 0";
+//         displayVal.style.maxHeight = "0";
+//         cssAttribute = false;
+//     }
+// }
